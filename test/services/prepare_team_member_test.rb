fad89d3411907f137682a372fc3b6f6ae36ef331
Bartosz Bonisławski <b.bonislawski@hotmail.com>
2016-08-04 21:17:46 +0200

    Create test for PrepareTeamMember

---
+++ test/services/prepare_team_member_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class PrepareTeamMemberTest < ActiveSupport::TestCase
+
+  test "creates team_member if doesnt exist" do
+    team = PrepareTeam.new.(team_params)
+    user_name = "user_name"
+    user_id = "user_id"
+
+    member = TeamMember.where(slack_user_name: user_name).first
+    assert(member.nil?)
+
+    PrepareTeamMember.new.(team, user_name, user_id)
+
+    member = TeamMember.where(slack_user_name: user_name).first
+    assert_not_nil(member)
+
+  end
+
+  private
+
+  def team_params
+    { team_id: "team_id", team_domain: "team_domain" }
+  end
+
+end
