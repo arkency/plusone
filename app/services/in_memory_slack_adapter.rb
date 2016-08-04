78b5643256b9f4859da20e19e19613bc3d9f67cb
Bartosz Bonisławski <b.bonislawski@hotmail.com>
2016-08-04 22:00:05 +0200

    Implement InMemorySlackAdapter

---
+++ app/services/in_memory_slack_adapter.rb
@@ -0,0 +1,23 @@
+class InMemorySlackAdapter
+
+  def initialize(team_slack_token)
+    @team_slack_token = team_slack_token
+  end
+
+  def get_real_user_name(user_tag)
+    if slack_username?(user_tag)
+      fetch_slack_username(user_tag)
+    else
+      user_tag
+    end
+  end
+
+  def fetch_slack_username(user_tag)
+    @team_slack_token == 'valid' ? user_tag[2..-3] : 'u'
+  end
+
+  def slack_username?(user_tag)
+    user_tag.start_with?("<@")
+  end
+
+end
