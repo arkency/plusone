cef3fe1ea03152ed4dedc017f96feabf6764b104
Bartosz Bonisławski <b.bonislawski@hotmail.com>
2016-08-19 23:53:13 +0200

    Add testing reason in message parser

--- test/services/message_parser_test.rb
+++ test/services/message_parser_test.rb
@@ -2,12 +2,13 @@ require 'test_helper'
 
 class MessageParserTest < ActiveSupport::TestCase
 
-  test "returns correct username" do
+  test "returns correct username and empty reason when not given" do
     trigger_word = "+1"
     recipient_name = "testuser"
     text = "#{trigger_word} #{recipient_name}"
     parser = MessageParser.new(text, trigger_word)
     result = parser.recipient_name
+    assert_empty(parser.reason)
     assert_equal(result, recipient_name)
   end
 
@@ -16,6 +17,16 @@ class MessageParserTest < ActiveSupport::TestCase
     parser = MessageParser.new(trigger_word, trigger_word)
     result = parser.recipient_name
     assert_empty(result)
+  end
+
+  test "returns correct reason if given" do
+    trigger_word = "+1"
+    recipient_name = "testuser"
+    reason = "for being nice to others"
+    text = "#{trigger_word} #{recipient_name} #{reason}"
+    parser = MessageParser.new(text, trigger_word)
+    result = parser.reason
+    assert_equal(reason, result)
   end
 
 end
