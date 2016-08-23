ab44dcb6e6576d95f3ad42ac3d91ae360be559f9
Bartosz Bonisławski <b.bonislawski@hotmail.com>
2016-08-23 22:12:38 +0200

    Implement GetGiftersStats

---
+++ app/services/get_gifters_stats.rb
@@ -0,0 +1,26 @@
+class GetGiftersStats
+  def call(team_params)
+    team = PrepareTeam.new.call(team_params)
+    data = fetch_data(team)
+    format(data)
+  end
+
+  private
+
+  def fetch_data team
+    team.team_members.includes(:given_pluses).map do |x| 
+      {
+        name: x.slack_user_name,
+        given_pluses: x.given_pluses.length
+      }
+    end
+  end
+
+  def format data
+    grouped_data = data.group_by { |x| x[:given_pluses] }
+    grouped_data.keys.sort.reverse_each.map do |key|
+       members = grouped_data[key].map{ |tm| tm[:name] }.join(', ')
+       "#{key}: #{members}"
+     end.join("\n")
+  end
+end
