87f43398ac8a95ad0cb591420c6d5cbcdeaec5b8
Bartosz Bonisławski <b.bonislawski@hotmail.com>
2016-08-23 23:09:49 +0200

    Add information about !gifters to instruction

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -40,6 +40,7 @@ class SlackController < ApplicationController
     "PlusOne bot instruction:\n" +
     "-Use '+1 @name' if you want to appreciate someone\n" +
     "-Use '+1 !stats' to get statistics\n" +
+    "-Use '+1 !gifters' to get gifters statistics\n" +
     "Want to help with PlusOne development? Feel welcome: https://github.com/arkency/plusone"
   end
 
