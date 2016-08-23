b5e6217995167eb5d8cb0c174a7614792daf28e1
Bartosz Bonisławski <b.bonislawski@hotmail.com>
2016-08-23 21:19:31 +0200

    Add Plus model with relations to team_member

---
+++ db/migrate/20160823191631_create_pluses.rb
@@ -0,0 +1,14 @@
+class CreatePluses < ActiveRecord::Migration
+  def change
+    create_table :pluses do |t|
+      t.references :sender, index: true
+      t.references :recipient, index: true
+      t.string :channel
+
+      t.timestamps null: false
+    end
+
+    add_foreign_key :pluses, :team_members, column: :sender_id
+    add_foreign_key :pluses, :team_members, column: :recipient_id
+  end
+end
