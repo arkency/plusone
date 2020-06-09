1ec5fe27b4e0d8f71d5fb6459eddbeb4fe5ccf63
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2020-06-09 23:57:05 +0200

    Better code grouping to allow splitting it into multiple services

--- app/services/plus_one.rb
+++ app/services/plus_one.rb
@@ -13,30 +13,38 @@ class PlusOne
 
   def call(params, team_params)
     ActiveRecord::Base.transaction do
-      slack_team = slack_team(team_params)
-
-      register_team(slack_team) unless team_exists?(slack_team)
+      register_team_if_needed(team_params)
 
       team = Team.find_by(slack_team_id: team_params[:team_id])
       sender, recipient = PrepareTransactionActors.new(team, SlackAdapter.new(team.slack_token)).call(params)
       recipient_name = MessageParser.new(params[:text], params[:trigger_word]).recipient_name
-      raise InvalidSlackToken if user_tag_which_is_not_an_alias?(recipient, recipient_name)
-      raise CannotPlusOneYourself if sender == recipient
-
       if Alias.exists?(user_alias: recipient_name)
         recipient = team.team_members.find_by(slack_user_name: Alias.find_by(user_alias: recipient_name).username)
       end
+      raise InvalidSlackToken if user_tag_which_is_not_an_alias?(recipient, recipient_name)
+      raise CannotPlusOneYourself if sender == recipient
+
+
       recipient.increment!(:points)
       Upvote.create(recipient: recipient, sender: sender)
 
-      return {
-        text: "#{sender.slack_user_name}(#{sender.points}) gave +1 for #{recipient.slack_user_name}(#{recipient.points})",
-        parse: "none"
-      }
+      return slack_output_message(recipient, sender)
     end
   end
 
   private
+
+  def slack_output_message(recipient, sender)
+    {
+      text: "#{sender.slack_user_name}(#{sender.points}) gave +1 for #{recipient.slack_user_name}(#{recipient.points})",
+      parse: "none"
+    }
+  end
+
+  def register_team_if_needed(team_params)
+    slack_team = slack_team(team_params)
+    register_team(slack_team) unless team_exists?(slack_team)
+  end
 
   def user_tag_which_is_not_an_alias?(recipient, recipient_name)
     (recipient.slack_user_name == 'u') && ! Alias.exists?(user_alias: recipient_name)
