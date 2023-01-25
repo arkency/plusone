1caab6685a4b71f3ad7a02c00febad7f8c9e105b
Paweł Pacana <pawel.pacana@gmail.com>
2023-01-25 22:30:57 +0100

    Lift dependency

--- app/models/plus_one.rb
+++ app/models/plus_one.rb
@@ -2,6 +2,10 @@ class PlusOne
   class CannotPlusOneYourself < StandardError
   end
 
+  def initialize(slack_adapter = SlackAdapter.new)
+    @slack_adapter = slack_adapter
+  end
+
   def call(user_name, text, trigger_word, team_id, team_domain)
     ActiveRecord::Base.transaction do
       team = Team.register(team_id, team_domain)
@@ -9,7 +13,7 @@ class PlusOne
 
       sender = PrepareSender.new.call(team_id, user_name)
       recipient =
-        PrepareRecipient.new(SlackAdapter.new).call(team_id, text, trigger_word)
+        PrepareRecipient.new(@slack_adapter).call(team_id, text, trigger_word)
 
       recipient.receive_upvote(sender)
 
