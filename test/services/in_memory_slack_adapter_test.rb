2c73c26480b1ae332405b7a053c2a4ee6fdfb8ba
Paweł Pacana <pawel.pacana@gmail.com>
2023-01-25 22:06:48 +0100

    Inline

--- test/services/in_memory_slack_adapter_test.rb
+++ test/services/in_memory_slack_adapter_test.rb
@@ -3,24 +3,17 @@ require "test_helper"
 class InMemorySlackAdapterTest < ActiveSupport::TestCase
   test "returns same username when it doesnt start with <@" do
     slack_adapter = InMemorySlackAdapter.new
-    user_name = "user_name"
-    real_user_name = slack_adapter.get_real_user_name(valid_token, user_name)
-    assert_equal user_name, real_user_name
+    assert_equal "user_name", slack_adapter.get_real_user_name(valid_token, "user_name")
   end
 
   test "returns u when user_name stats with <@ and token is not valid" do
     slack_adapter = InMemorySlackAdapter.new
-    user_name = "<@user_name>"
-    real_user_name = slack_adapter.get_real_user_name(invalid_token, user_name)
-    assert_equal "u", real_user_name
+    assert_equal "u", slack_adapter.get_real_user_name(invalid_token, "<@user_name>")
   end
 
   test "returns username without <@ when token is valid" do
     slack_adapter = InMemorySlackAdapter.new
-    user_name = "<@user_name>"
-    expected_user_name = "user_name"
-    real_user_name = slack_adapter.get_real_user_name(valid_token, user_name)
-    assert_equal expected_user_name, real_user_name
+    assert_equal "user_name", slack_adapter.get_real_user_name(valid_token,  "<@user_name>")
   end
 
   private
