62d017d9148f7f2a510fd64fc5f5298172debab1
Paweł Pacana <pawel.pacana@gmail.com>
2023-01-25 22:04:51 +0100

    Refactor

--- test/services/in_memory_slack_adapter_test.rb
+++ test/services/in_memory_slack_adapter_test.rb
@@ -4,14 +4,14 @@ class InMemorySlackAdapterTest < ActiveSupport::TestCase
   test "returns same username when it doesnt start with <@" do
     slack_adapter = InMemorySlackAdapter.new
     user_name = "user_name"
-    real_user_name = slack_adapter.get_real_user_name("valid", user_name)
+    real_user_name = slack_adapter.get_real_user_name(valid_token, user_name)
     assert_equal(user_name, real_user_name)
   end
 
   test "returns u when user_name stats with <@ and token is not valid" do
     slack_adapter = InMemorySlackAdapter.new
     user_name = "<@user_name>"
-    real_user_name = slack_adapter.get_real_user_name("invalid", user_name)
+    real_user_name = slack_adapter.get_real_user_name(invalid_token, user_name)
     assert_equal("u", real_user_name)
   end
 
@@ -19,7 +19,17 @@ class InMemorySlackAdapterTest < ActiveSupport::TestCase
     slack_adapter = InMemorySlackAdapter.new
     user_name = "<@user_name>"
     expected_user_name = "user_name"
-    real_user_name = slack_adapter.get_real_user_name("valid", user_name)
+    real_user_name = slack_adapter.get_real_user_name(valid_token, user_name)
     assert_equal(expected_user_name, real_user_name)
+  end
+
+  private
+
+  def valid_token
+    "valid"
+  end
+
+  def invalid_token
+    "invalid"
   end
 end
