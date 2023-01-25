227f76a6961713aee640ce51e8b7c4ae29e83849
Łukasz Reszke <lukaszreszke93@gmail.com>
2023-01-25 18:44:28 +0100

    Inline methods
    due to recent removals it's easier to read
    
    Co-authored-by: Paweł Pacana <pawel.pacana@gmail.com>
    Co-authored-by: Szymon Fiedler <szymon.fiedler@gmail.com>

--- app/services/plus_one.rb
+++ app/services/plus_one.rb
@@ -4,11 +4,11 @@ class PlusOne
 
   def call(params, team_params)
     ActiveRecord::Base.transaction do
-      register_team_if_needed(team_params)
-      register_sender_if_needed(
-        team_params.fetch(:team_id),
-        params.fetch(:user_name)
+      Team.find_or_create_by(
+        slack_team_id: team_params.fetch(:team_id),
+        slack_team_domain: team_params.fetch(:team_domain)
       )
+      RegisterTeamMember.new.call(team_params.fetch(:team_id), params.fetch(:user_name))
 
       sender =
         PrepareSender.new.call(
@@ -28,26 +28,5 @@ class PlusOne
 
       SlackMessages.slack_output_message(recipient, sender)
     end
-  end
-
-  private
-
-  def register_team_if_needed(team_params)
-    register_team(team_params)
-  end
-
-  def register_sender_if_needed(team_id, user_name)
-    register_team_member(user_name, team_id)
-  end
-
-  def register_team_member(user_name, team_id)
-    RegisterTeamMember.new.call(team_id, user_name)
-  end
-
-  def register_team(team_params)
-    Team.find_or_create_by(
-      slack_team_id: team_params.fetch(:team_id),
-      slack_team_domain: team_params.fetch(:team_domain)
-    )
   end
 end
