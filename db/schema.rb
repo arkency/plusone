15ca14991c2d641e03509a7fcb5eb4bafc5d2bb2
Łukasz Reszke <lukaszreszke93@gmail.com>
2023-01-25 17:44:07 +0100

    Add plpgsql extension generated by ActiveRecord migration
    Co-authored-by: Paweł Pacana <pawel.pacana@gmail.com>
    Co-authored-by: Szymon Fiedler <szymon.fiedler@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_12_03_220435) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "aliases", force: :cascade do |t|
     t.string "username"
     t.string "user_alias"
