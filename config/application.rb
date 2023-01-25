a619dc1fdf64492376ffee8c398a2bb98e593dd2
Paweł Pacana <pawel.pacana@gmail.com>
2023-01-25 22:19:50 +0100

    Autoload lib as well

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,6 @@ module Plusone
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.eager_load_paths << Rails.root.join("lib").to_s
   end
 end
