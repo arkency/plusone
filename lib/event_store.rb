b46be0fae29821c56a1bdf50a2482051ecd8297c
Paweł Pacana <pawel.pacana@gmail.com>
2023-01-26 16:31:37 +0100

    Inline upstream mapper
    We'll be adding some new transformations to the pipeline in next
    commits.

--- lib/event_store.rb
+++ lib/event_store.rb
@@ -1,3 +1,37 @@
 require "rails_event_store"
 
-EventStore = Class.new(RailsEventStore::JSONClient)
+class EventStore < RailsEventStore::JSONClient
+  def initialize
+    super(mapper: mapper)
+  end
+
+  private
+
+  def mapper
+    RubyEventStore::Mappers::InstrumentedMapper.new(
+      RubyEventStore::Mappers::PipelineMapper.new(
+        RubyEventStore::Mappers::Pipeline.new(
+          RubyEventStore::Mappers::Transformation::PreserveTypes
+            .new
+            .register(Symbol, serializer: ->(v) { v.to_s }, deserializer: ->(v) { v.to_sym })
+            .register(
+              Time,
+              serializer: ->(v) { v.iso8601(RubyEventStore::TIMESTAMP_PRECISION) },
+              deserializer: ->(v) { Time.iso8601(v) }
+            )
+            .register(
+              ActiveSupport::TimeWithZone,
+              serializer: ->(v) { v.iso8601(RubyEventStore::TIMESTAMP_PRECISION) },
+              deserializer: ->(v) { Time.iso8601(v).in_time_zone },
+              stored_type: ->(*) { "ActiveSupport::TimeWithZone" }
+            )
+            .register(Date, serializer: ->(v) { v.iso8601 }, deserializer: ->(v) { Date.iso8601(v) })
+            .register(DateTime, serializer: ->(v) { v.iso8601 }, deserializer: ->(v) { DateTime.iso8601(v) })
+            .register(BigDecimal, serializer: ->(v) { v.to_s }, deserializer: ->(v) { BigDecimal(v) }),
+          RubyEventStore::Mappers::Transformation::SymbolizeMetadataKeys.new
+        )
+      ),
+      ActiveSupport::Notifications
+    )
+  end
+end
