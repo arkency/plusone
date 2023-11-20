1982d565ba47271d62b75013a94bf76120cbcb7d
Szymon Fiedler <szymon.fiedler@gmail.com>
2023-11-20 11:23:17 +0100

    Extract helper method

--- test/integration/aliases_test.rb
+++ test/integration/aliases_test.rb
@@ -6,27 +6,24 @@ class AliasesTest < ActionDispatch::IntegrationTest
   cover SlackAliasMessageParser
 
   def test_only_allow_user_tags_as_aliases
-    post "/slack/plus",
-         params: webhook_params(trigger_word: "+1", text: "+1 user_name2", user_name: "user_name1", user_id: "user_id1")
-
+    add_points
     alias_user_name("user_name2", "<@U026BA51D>")
-
     alias_user_name("user_name2", "new_alias")
 
     assert_equal("Invalid user tag", response_text)
   end
 
   def test_can_alias_different_users
-    post "/slack/plus",
-         params: webhook_params(trigger_word: "+1", text: "+1 user_name2", user_name: "user_name1", user_id: "user_id1")
+    add_points
 
-    alias_user_name("user_name1", "<@U055CC67F>")
-    alias_user_name("user_name2", "<@U026BA51D>")
+    assert_nothing_raised do
+      alias_user_name("user_name1", "<@U055CC67F>")
+      alias_user_name("user_name2", "<@U026BA51D>")
+    end
   end
 
   def test_can_alias_with_exotic_trigger_word
-    post "/slack/plus",
-         params: webhook_params(trigger_word: "+1", text: "+1 user_name2", user_name: "user_name1", user_id: "user_id1")
+    add_points
 
     post "/slack/plus",
          params: webhook_params(text: "dej plusika dla !alias user_name2 <@U026BA51D>", trigger_word: "dej plusika dla")
@@ -41,16 +38,8 @@ class AliasesTest < ActionDispatch::IntegrationTest
   end
 
   def test_stats_show_aliases_together
-    post "/slack/plus",
-         params: webhook_params(trigger_word: "+1", text: "+1 user_name2", user_name: "user_name1", user_id: "user_id1")
-    post "/slack/plus",
-         params:
-           webhook_params(
-             trigger_word: "+1",
-             text: "+1 !alias user_name2 <@U026BA51D>",
-             user_name: "user_name1",
-             user_id: "user_id1",
-           )
+    add_points
+    alias_user_name("user_name2", "<@U026BA51D>")
 
     assert_equal(response_text, "<@U026BA51D> is now an alias to user_name2")
 
@@ -66,9 +55,7 @@ class AliasesTest < ActionDispatch::IntegrationTest
   end
 
   def test_cant_alias_existing_alias
-    post "/slack/plus",
-         params: webhook_params(trigger_word: "+1", text: "+1 user_name2", user_name: "user_name1", user_id: "user_id1")
-
+    add_points
     alias_user_name("user_name2", "<@U026BA51D>")
 
     assert_raises AliasToUserTag::AlreadyExists do
@@ -77,9 +64,7 @@ class AliasesTest < ActionDispatch::IntegrationTest
   end
 
   def test_cant_plusone_your_own_alias
-    post "/slack/plus",
-         params: webhook_params(trigger_word: "+1", text: "+1 user_name2", user_name: "user_name1", user_id: "user_id1")
-
+    add_points
     alias_user_name("user_name2", "<@U026BA51D>")
 
     post "/slack/plus",
@@ -90,6 +75,11 @@ class AliasesTest < ActionDispatch::IntegrationTest
   end
 
   private
+
+  def add_points
+    post "/slack/plus",
+         params: webhook_params(trigger_word: "+1", text: "+1 user_name2", user_name: "user_name1", user_id: "user_id1")
+  end
 
   def alias_user_name(username, user_alias)
     post "/slack/plus", params: webhook_params(text: "+1 !alias #{username} #{user_alias}", trigger_word: "+1")
