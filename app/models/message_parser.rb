315cfdf896e3beacca0bdc99ffe1e28ee5b00bea
Szymon Fiedler <szymon.fiedler@gmail.com>
2023-11-20 12:21:22 +0100

    A bit more idiomatic syntax for Ruby

--- app/models/message_parser.rb
+++ app/models/message_parser.rb
@@ -1,7 +1,7 @@
 class MessageParser
   def initialize(text, trigger_word)
-    self.text = text
-    self.trigger_word = trigger_word
+    @text = text
+    @trigger_word = trigger_word
   end
 
   def recipient_name
